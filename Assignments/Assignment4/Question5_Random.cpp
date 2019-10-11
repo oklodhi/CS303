36ec021ba0ae63d5351b97a73ee34c5e76289dde
Omer Khan <okhanlodhi@gmail.com>
2019-10-10 23:11:47 -0500

    Selection, bubble, insertion, shell

--- Assignments/Assignment4/Question5_Random.cpp
+++ Assignments/Assignment4/Question5_Random.cpp
@@ -1,290 +1,195 @@
 #include <iostream>
-#include <ctime>
 #include <cstdlib>
-#include <vector>
-#include <list>
+#include <ctime>
+#include <string>
 
 using namespace std;
 
-// Random sorting stuff
+// constant variable declarations
+const int SIZE = 10000; 
+const int TYPE_RANDOM = 0;
+const int TYPE_SORTED = 1; 
+const int TYPE_REVERSE = 2; 
 
-void generate_array(int arr[], int size){
-    srand(time(NULL));
-    
-    for(int i = 0; i < size; i++){
-        arr[i] = rand() % 10000 + 1; 
-        cout << arr[i] << " "; 
-    }
+// global counters for comparisons and exchanges
+int comparisons = 0;
+int exchanges = 0;
+
+void set_to_zero() {
+	comparisons = 0;
+	exchanges = 0;
 }
 
-void ShellSort(vector <int> &num) {
-	int i, temp, flag = 1, numLength = num.size();
-	int gap = numLength;
-	bool insertionNeeded = false;
-	int swapPos = 0;
-	int key;
+// generate the arrays of SIZE with depending on RANDOM, ASCENDING, and DESCENDING
+void generate_arrays(int arr[], int size, int arrtype) {
+	if (arrtype == 0) { // random fill
+		srand(time(NULL));
+		for (int i = 0; i < size; i++) {
+			// fill from 1 - 10000 randomly
+			arr[i] = rand() % size + 1; 
+			//cout << arr[i] << " ";
+		}
+		//cout << endl << endl;
+	}
+	else if (arrtype == 1) { // asceding sorted fill
+		for (int i = 0; i < size; i++) {
+			// fill from 1 - 10000
+			arr[i] = i+1;
+			//cout << arr[i] << " ";
+		}
+		//cout << endl << endl;
+	}
+	else if (arrtype == 2) { // reverse sorted fill
+		for (int i = size; i > 0; i--) {
+			// fill from 10000 - 1
+			arr[i] = i;
+			//cout << arr[i] << " ";
+		}
+		//cout << endl << endl;
+	}
+}
 
-	while (true)  {    // boolean flag (true when not equal to 0) 
-		gap = (gap - 1) / 2;
-		if (gap == 0)
-			break;
+// swapping elements
+void swap(int arr[], int i, int j) {
+	int temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp; 
+}
 
-		for (int k = 0; k < gap; k++) {
-			for (int j = gap + k; j < numLength; j += gap) {
-				key = num[j];
+// selection sort algo
+void selectionSort(int arr[], int size) {
+	for (int i = 0; i < size - 1; i++) {
+		int min = i;
+		for (int j = i + 1; j < size; j++) {
+			comparisons++; 
+			if (arr[j] < arr[i]) {
+				min = j;
+			}
+		}
+		exchanges++; 
+		swap(arr, min, i);
+	}
+}
 
-				insertionNeeded = false;
-				for (i = j - gap; i >= 0; i -= gap) {   // Smaller values move right
+// bubble sort algo
+void bubbleSort(int arr[], int size) {
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = i + 1; j < size; j++) {
+			comparisons++;
+			if (arr[i] < arr[j]) {
+				exchanges++;
+				swap(arr, i, j);
+			}
+		}
+	}
+}
 
-					if (key < num[i]) {
-						swapPos = i;
-						num[i + gap] = num[i];
-						insertionNeeded = true;
-					}
-					else
-						break;
+// insertion sort algo
+void insertionSort(int arr[], int size) {
+	for (int i = 1; i < size; i++) {
+		int temp = arr[i];
+		int j = i; 
+
+		comparisons++;
+		while (j > 0 && temp < arr[j - 1]) {
+			exchanges++;
+			arr[j] = arr[j - 1];
+			j--;
+		}
+
+		arr[j] = temp;
+	}
+}
+
+// shell sort algo
+void shellSort(int arr[], int size) {
+	int x = size / 2;
+	while (x > 0)
+	{
+		for (int i = x; i < size; i++)
+		{
+			for (int j = i; j >= x; j = j - x)
+			{
+				comparisons++;
+				if (arr[j] < arr[j - x])
+				{
+					exchanges++;
+					swap(arr, j, j - x);
 				}
-				if (insertionNeeded) {
-					num[swapPos] = key;    //Put key into its proper location
+				else
+				{
+					break;
 				}
 			}
 		}
-	}
-	return;
-}
 
-
-void selection_sort(vector<int>& vec){
-
-	for (int fill = 0; fill != vec.size(); ++fill) {
-		int pos_min = fill;
-		for (int next = fill + 1; next != vec.size(); ++next) {
-
-			if (vec[next] < vec[pos_min])
-				pos_min = next;
-
-		}
-		if (fill != pos_min)
-			swap(vec[pos_min], vec[fill]);
-
+		x = x / 2;
 	}
 }
 
 
-void BubbleSort(vector <int> &num){
+int main() {
+	// initiate arrays
+	int random_array[SIZE]; 
+	int sorted_array[SIZE];
+	int reverse_array[SIZE]; 
 
-	for (int i = 1; i <= num.size(); i++){
-		for (int j = 0; j < num.size() - i; j++){
-			if (num[j + 1] < num[j])
-				swap(num[j], num[j + 1]);// swap elements
-		}
-	}
-}
+	// generate arrays with filled data
+	generate_arrays(random_array, SIZE, 0);
+	generate_arrays(sorted_array, SIZE, 1);
+	generate_arrays(reverse_array, SIZE, 2);
 
-void bubble_sort_improved(vector <int> &vec){
-	bool exchanges = false;
-	for (int i = 1; i <= vec.size(); i++){
-		exchanges = false;
-		for (int j = 0; j < vec.size() - i; j++){
-			if (vec[j + 1] < vec[j]){
-				swap(vec[j], vec[j + 1]);// swap elements
-				exchanges = true;
-			}
-		}
-		if (!exchanges)
-			break;
-	}
-}
+	/*----------------------------------------------*/
 
-void bubble_sort_list(list<int> &li){
-	bool exchanges = false;
-	list<int>::iterator itr = li.begin();
-	for (int i = 1; i <= li.size(); i++){
-		exchanges = false;
-		itr = li.begin();
-		for (int j = 0; j < li.size() - i; j++){
-			list<int>::iterator current = itr;
-			if (*(++itr)< *current){
-				swap(*current, *itr);// swap elements
-				exchanges = true;
-			}
-		}
-		if (!exchanges)
-			break;
-	}
-}
+	// SELECTION SORT COMPARISON AND EXCHANGES
+	selectionSort(random_array, SIZE);
+	cout << "Selection Sort (Random) C: " << comparisons << "\tE: " << exchanges << endl;
+	set_to_zero();
 
-void insertion_sort(std::vector <int> &num) {
-	int i, j, key;
-	bool insertionNeeded = false;
+	selectionSort(sorted_array, SIZE);
+	cout << "Selection Sort (Ascend) C: " << comparisons << "\tE: " << exchanges << endl;
+	set_to_zero();
 
-	for (j = 1; j < num.size(); j++) {
-		key = num[j];
-		insertionNeeded = false;
-		for (i = j - 1; i >= 0; i--){ // larger values move right
+	selectionSort(reverse_array, SIZE);
+	cout << "Selection Sort (Descen) C: " << comparisons << "\tE: " << exchanges << endl;
+	set_to_zero();
 
-			if (key < num[i]) {
-				num[i + 1] = num[i];
-				insertionNeeded = true;
-			}
-			else
-				break;
-		}
-		if (insertionNeeded)
-			num[i + 1] = key;    //Put key into its proper location
-	}
-}
+	// BUBBLE SORT COMPARISON AND EXCHANGES
+	bubbleSort(random_array, SIZE);
+	cout << "Bubble Sort (Random) C: " << comparisons << "\tE: " << exchanges << endl;
+	set_to_zero();
 
+	bubbleSort(sorted_array, SIZE);
+	cout << "Bubble Sort (Ascend) C: " << comparisons << "\tE: " << exchanges << endl;
+	set_to_zero();
 
-void merge(std::vector<int>& array, std::vector<int>& result, int lowPointer, int highPointer, int upperBound) {
+	bubbleSort(reverse_array, SIZE);
+	cout << "Bubble Sort (Descen) C: " << comparisons << "\tE: " << exchanges << endl;
+	set_to_zero();
 
-	int j = 0;
-	int lowerBound = lowPointer;
-	int mid = highPointer - 1;
-	int n = upperBound - lowerBound + 1; //number of items
+	// INSERTION SORT COMPARISON AND EXCHANGES
+	insertionSort(random_array, SIZE);
+	cout << "Insertion Sort (Random) C: " << comparisons << "\tE: " << exchanges << endl;
+	set_to_zero();
 
-	while (lowPointer <= mid && highPointer <= upperBound){
+	insertionSort(sorted_array, SIZE);
+	cout << "Insertion Sort (Ascend) C: " << comparisons << "\tE: " << exchanges << endl;
+	set_to_zero();
 
-		if (array[lowPointer] < array[highPointer])
-			result[j++] = array[lowPointer++];
-		else
-			result[j++] = array[highPointer++];
-	}
+	insertionSort(reverse_array, SIZE);
+	cout << "Insertion Sort (Descen) C: " << comparisons << "\tE: " << exchanges << endl;
+	set_to_zero();
 
-	while (lowPointer <= mid) 
-		result[j++] = array[lowPointer++];
+	// SHELL SORT COMPARISON AND EXCHANGES
+	shellSort(random_array, SIZE);
+	cout << "Shell Sort (Random) C: " << comparisons << "\tE: " << exchanges << endl;
+	set_to_zero();
 
+	shellSort(sorted_array, SIZE);
+	cout << "Shell Sort (Ascend) C: " << comparisons << "\tE: " << exchanges << endl;
+	set_to_zero();
 
-	while (highPointer <= upperBound) 
-		result[j++] = array[highPointer++];
-	
-
-	for (j = 0; j < n; j++) //copy the items from the temporary array to the original array
-		array[lowerBound + j] = result[j];
-}
-
-void mergesort(std::vector<int>& array, std::vector<int>& result, int lowerBand, int upperBand) {
-	int midpoint;
-	if (lowerBand < upperBand) {
-		midpoint = (lowerBand + upperBand) / 2;
-		mergesort(array, result, lowerBand, midpoint); //merge sort the left half
-		mergesort(array, result, midpoint + 1, upperBand); //merge sort the right half
-		merge(array, result, lowerBand, midpoint + 1, upperBand);
-	}
-}
-
-void mergeSort(std::vector<int>& array){
-	std::vector<int> result = array;
-	mergesort(array, result, 0, array.size() - 1);
-}
-
-
-
-int partition(int first, int last, std::vector<int>& arr) {
-	// Start up and down at either end of the sequence.
-	// The first table element is the pivot value.
-	int up = first+1;
-	int down = last-1;
-	do {
-
-		while ((up != last) && !(arr[first] < arr[up])) {
-			++up;
-		}
-		while (arr[first] < arr[down]) {
-			--down;
-		}
-		if (up < down) {
-			// if up is to the left of down,
-			swap(arr[up], arr[down]);
-		}
-	} while (up < down); // Repeat while up is left of down.
-
-	swap(arr[down], arr[first]);
-	return down;
-}
-
-void sort_median(std::vector<int>& arr, int first, int last){
-
-	bool exchanges = true;
-	int middle = (first + last) / 2;
-	if (arr[first]>arr[middle])
-		swap(arr[first], arr[middle]);
-	if (arr[middle] > arr[last])
-		swap(arr[middle], arr[last]);
-	if (arr[first] > arr[middle])
-		swap(arr[first], arr[middle]);
-
-
-	//swap the middle with the left 
-	swap(arr[middle], arr[first]);
-}
-
-int partitionB(int first, int last, std::vector<int>& arr) {
-	// Start up and down at either end of the sequence.
-	// The first table element is the pivot value.
-
-
-	int up = first+1;
-	int down = last-1;
-	sort_median(arr, first, last-1);
-	int mid = first;
-	do {
-		while ((up != last - 1) && !(arr[mid] < arr[up])) {
-			++up;
-		}
-		while (arr[mid] < arr[down]) {
-			--down;
-		}
-		if (up < down) {
-			// if up is to the left of down,
-			swap(arr[up], arr[down]);
-		}
-	} while (up < down); // Repeat while up is left of down.
-
-	swap(arr[mid], arr[down]);
-	
-	return down;
-}
-
-void middle_quick_sort(int first, int last, std::vector<int>& arr) {
-	if (last - first > 1) {
-		// There is data to be sorted.
-		// Partition the table.
-		int pivot = partitionB(first, last, arr);
-
-		// Sort the left half.
-		middle_quick_sort(first, pivot, arr);
-
-		// Sort the right half.
-		middle_quick_sort(pivot + 1, last, arr);
-	}
-}
-
-void middle_quick_sort_wrapper(vector<int>& arr){
-	middle_quick_sort(0, arr.size(), arr);
-}
-
-void quick_sort(int first, int last, std::vector<int>& arr) {
-	if (last - first > 1) {
-		// There is data to be sorted.
-		// Partition the table.
-		int pivot = partition(first, last, arr);
-
-		// Sort the left subarray.
-		quick_sort(first, pivot, arr);
-
-		// Sort the right subarray.
-		quick_sort(pivot + 1, last, arr);
-	}
-}
-
-void quick_sort_wrapper(vector<int>& arr){
-	quick_sort(0, arr.size(), arr);
-}
-
-int main(){
-    const int size = 10000;
-    int randomArr[size];
-    
-    generate_array(randomArr, size);
+	shellSort(reverse_array, SIZE);
+	cout << "Shell Sort (Descen) C: " << comparisons << "\tE: " << exchanges << endl;
+	set_to_zero();
 }
